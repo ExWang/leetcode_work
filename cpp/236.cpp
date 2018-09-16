9d8e32105c79b073ca2a91eae4d7d5f54003c8f8
Nebula Wang <wangshihao1117@hotmail.com>
2018-09-16 23:09:56 +0800

    Create 236.cpp

---
+++ cpp/236.cpp
@@ -0,0 +1,24 @@
+//236. Lowest Common Ancestor of a Binary Tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root==p || root==q)
+            return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (!left) return right;
+        else if (!right) return left;
+        else return root;
+        // return !left ? right : !right ? left : root;
+        // return root if left and right else left or right
+    }
+};
