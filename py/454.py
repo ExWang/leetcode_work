27213bad2cd5f33c811c67cc8f0f59b70399faf4
Nebula Wang <wangshihao1117@hotmail.com>
2018-12-26 22:56:07 +0800

    Update 454.py

--- py/454.py
+++ py/454.py
@@ -21,4 +21,38 @@ class Solution:
                 if -(c+d) in mydict:
                     count += mydict[-(c+d)]
         
-        return count
+        return coun
+
+# Fastest
+class Solution_fast:
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        cnts = 0
+        dd = {}
+        aa = {}
+        bb = {}
+        cc = {}
+        for a in A:
+            aa[a] = aa.get(a, 0) + 1
+        for b in B:
+            bb[b] = bb.get(b, 0)+1
+        for c in C:
+            cc[c] = cc.get(c, 0) + 1
+        for d in D:
+            dd[d] = dd.get(d, 0) + 1
+        ab = {}
+        for a, av in aa.items():
+            for b, bv in bb.items():
+                ab[a+b] = ab.get(a+b, 0) + av*bv
+
+        for c, cv in cc.items():
+            for d, dv in dd.items():
+                if -c-d in ab:
+                    cnts += ab[-c-d] * cv * dv
+        return cnts
